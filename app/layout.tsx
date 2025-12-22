3540f7cd32e989ac7569bf35f7a59294afddb06c
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-12-22 06:36:24 +0530

    Refactor code structure for improved readability and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Finance Tracer",
   description: "AI Intergrated Finance Tracer",
+  icons: {
+    icon: "/icon.png",
+  },
 };
 
 export default function RootLayout({
